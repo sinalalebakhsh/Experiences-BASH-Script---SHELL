91bf5c7db8fd202e4df295a3cf1061aee8bfdbb2
Iranian Programer <93151188+sinalalebakhsh@users.noreply.github.com>
2022-05-16 18:29:11 +0430

    Add files via upload

---
+++ 2022/1- First Learning/2- Second Learning.sh
@@ -0,0 +1,21 @@
+#!
+
+name="Sinanis"
+
+echo "Hi Friend :) my name is $name!!"
+
+sleep 2
+
+echo "Do you know the $name!! is Palindrome name?"
+
+sleep 2 
+
+echo "Yeah!! is Palindrome :)"
+
+sleep 2 
+
+echo "Do you any Palindrome name?"
+
+
+sleep 4
+
